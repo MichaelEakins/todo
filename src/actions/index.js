aed5409f78b7345adf1b67242304ef2e1c5184e9
Michael Eakins <michaelj.eakins@gmail.com>
2019-06-26 13:15:06 -0400

    Added actionTypes

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,14 @@
+import {ADD_TODO, TOGGLE_TODO} from '../actions/actionTypes'
+
 let nextId = 0
 
 export const addTodo = (test) => ({
-    type: 'ADD_TODO', 
+    type: ADD_TODO, 
     id: nextId++,
     text
 })
 
 export const toggleTodo = (id) => ({
-    type: 'TOGGLE_TODO',
+    type: TOGGLE_TODO,
     id
 })
